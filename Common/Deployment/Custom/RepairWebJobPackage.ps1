0095ebe58e673908b59bc06b4a79ea5ad02afb92
Thomas Tomow <toto_san@live.com>
2016-07-17 14:39:38 +0200

    fixed pscript to include zip file assembly

--- Common/Deployment/Custom/RepairWebJobPackage.ps1
+++ Common/Deployment/Custom/RepairWebJobPackage.ps1
@@ -2,7 +2,7 @@
         [Parameter(Mandatory=$true,Position=0)] [string] $filePath
     )
 
-
+	Add-Type -Path "C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.IO.Compression.FileSystem.dll"
    
     $zipfile = get-item $filePath
     $zip = [System.IO.Compression.ZipFile]::Open($zipfile.FullName, "Update")
